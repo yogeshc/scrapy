88a896770f935fb1854f78303ca896e4fae94a81
Yogesh <mr.yogesh@gmail.com>
2013-04-29 12:41:42 +0530

    change import unittest to from django.utils import unittest

--- scrapy/tests/test_contrib_loader.py
+++ scrapy/tests/test_contrib_loader.py
@@ -1,4 +1,4 @@
-import unittest
+from django.utils import unittest
 
 from scrapy.contrib.loader import ItemLoader, XPathItemLoader
 from scrapy.contrib.loader.processor import Join, Identity, TakeFirst, \
